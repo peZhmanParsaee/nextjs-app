7486e2d59c7c626495f3665f02909fda41760119
Pezhman Parsaee <pezhmanparsaee@Pezhmans-MacBook-Pro.local>
2022-08-10 14:19:46 +0200

    modify

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import { useEffect } from "react"
+import { getCookie, setCookie } from "cookies-next"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const value = getCookie("sample-cookie")
+    console.log(`
+      env: ${typeof window != "undefined" ? "client": "server"}
+      cookie value: ${value}
+    `)
+  })
+
   return <Component {...pageProps} />
+}
+
+MyApp.getInitialProps = ({ ctx }) => {
+  setCookie("sample-cookie", "1234", ctx)
+
+  return {}
 }
 
 export default MyApp
